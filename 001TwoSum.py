bae35862309944dfbab62736b3168a919a349f18
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-08-28 17:49:37 -0400

    Update 001TwoSum.py

--- 001TwoSum.py
+++ 001TwoSum.py
@@ -5,18 +5,9 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        nums_with_index = [(index, value) for value, index in enumerate(nums)]
-        nums_with_index.sort()
-        start, end  = 0, len(nums) - 1
-        while start < end:
-            curr_sum = nums_with_index[start][0] + nums_with_index[end][0]
-            if curr_sum == target:
-                return (nums_with_index[start][1], nums_with_index[end][1])
-            elif curr_sum < target:
-                start += 1
-                pass
+        dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] not in dict:
+                dict[nums[i]]=i
             else:
-                end -= 1
-                pass
-            pass
-        return None
+                return [dict[target-nums[i]],i]
