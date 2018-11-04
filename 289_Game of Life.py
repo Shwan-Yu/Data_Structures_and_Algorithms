1bc97ac5126b05f16202988017280c30d1121a63
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-11-04 14:57:58 -0500

    Update 289_Game of Life.py

--- 289_Game of Life.py
+++ 289_Game of Life.py
@@ -4,23 +4,24 @@ class Solution:
         :type board: List[List[int]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
+        m, n = len(board), len(board[0])
         def liveNeighbors(board, i, j):
-            directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+            pos = [(0,1),(0,-1),(1,0),(-1,0),(-1,-1),(1,-1),(-1,1),(1,1)]
             count = 0
-            for d in directions:
-                ni, nj = d[0]+i, d[1]+j
-                if 0 <= ni <= len(board)-1 and 0 <= nj <= len(board[0])-1:
+            for p in pos:
+                ni, nj = i + p[0], j + p[1]
+                if 0 <= ni < m and 0 <= nj < n:
                     count += (board[ni][nj] & 1)
             return count
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                n = liveNeighbors(board, i, j)
-                if board[i][j] == 1 and (n == 2 or n == 3):
+            
+        for i in range(m):
+            for j in range(n):
+                count = liveNeighbors(board, i, j)
+                if board[i][j] == 1 and (count == 2 or count == 3):
                     board[i][j] = 3
-                elif board[i][j] == 0 and n == 3:
+                elif board[i][j] == 0 and count == 3:
                     board[i][j] = 2
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+        for i in range(m):
+            for j in range(n):
                 board[i][j] >>= 1
