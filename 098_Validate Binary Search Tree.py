86bdaae3c124e24c343d933e156501474f7464e1
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-11-06 18:40:30 -0500

    Update 098_Validate Binary Search Tree.py

--- 098_Validate Binary Search Tree.py
+++ 098_Validate Binary Search Tree.py
@@ -1,24 +1,21 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution(object):
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        
-        def check_BST(node, left_sub, right_sub):
+        def isValid(node, l_boundary, r_boundary):
             if not node:
                 return True
-        
-            if not left_sub < node.val < right_sub:
+            if not l_boundary < node.val < r_boundary:
                 return False
-            
-            return check_BST(node.left, left_sub, node.val) and check_BST(node.right, node.val, right_sub)
+            return isValid(node.left, l_boundary, node.val) and isValid(node.right, node.val, r_boundary)
         
-        return check_BST(root, float("-inf"), float("inf"))
+        return isValid(root, float("-inf"), float("inf"))
