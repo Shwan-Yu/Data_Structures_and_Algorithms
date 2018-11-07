68330f24c1dfc59aa0747aa28d5044db19f1fd15
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-11-07 18:03:00 -0500

    Update 617_Merge Two Binary Trees.py

--- 617_Merge Two Binary Trees.py
+++ 617_Merge Two Binary Trees.py
@@ -16,9 +16,9 @@ class Solution(object):
             if not t1 or not t2:
                 return t1 or t2
             
-            t3 = TreeNode(t1.val + t2.val)
-            t3.left = dfs(t1.left, t2.left)
-            t3.right = dfs(t1.right, t2.right)
-            return t3
+            t1.val += t2.val
+            t1.left = dfs(t1.left, t2.left)
+            t1.right = dfs(t1.right, t2.right)
+            return t1
         
         return dfs(t1, t2)
