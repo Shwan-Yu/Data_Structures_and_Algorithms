5e5487887da953cda2a479bd4a6731e29e5ca493
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-11-09 15:39:25 -0500

    Update 098_Validate Binary Search Tree.py

--- 098_Validate Binary Search Tree.py
+++ 098_Validate Binary Search Tree.py
@@ -5,17 +5,47 @@
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def isValidBST(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        def isValid(node, l_boundary, r_boundary):
-            if not node:
-                return True
-            if not l_boundary < node.val < r_boundary:
+        stack, node = [], root
+        prev = TreeNode(float("-inf"))
+        while node:
+            stack.append(node)
+            node = node.left
+        while stack:
+            node = stack.pop()
+            if node.val <= prev.val:
                 return False
-            return isValid(node.left, l_boundary, node.val) and isValid(node.right, node.val, r_boundary)
+            prev = node
+            node = node.right
+            while node:
+                stack.append(node)
+                node = node.left
         
-        return isValid(root, float("-inf"), float("inf"))
+        return True
+        
+# class Solution(object):
+#     def isValidBST(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: bool
+#         """
+#         def isValid(node, l_boundary, r_boundary):
+#             if not node:
+#                 return True
+#             if not l_boundary < node.val < r_boundary:
+#                 return False
+#             return isValid(node.left, l_boundary, node.val) and isValid(node.right, node.val, r_boundary)
+        
+#         return isValid(root, float("-inf"), float("inf"))
