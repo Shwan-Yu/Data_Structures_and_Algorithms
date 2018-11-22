5c6868348970b84accae61591ba247e338bb5888
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-11-22 18:26:50 -0500

    Update 416_Partition Equal Subset Sum.py

--- 416_Partition Equal Subset Sum.py
+++ 416_Partition Equal Subset Sum.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def canPartition(self, nums):
-        cache = {}
+        memo = set()
         def helper(start, target):         # Here path is not needed
-            if (start, target) in cache:
-                return cache[(start, target)]
+            if (start, target) in memo:
+                return False
             if target < 0:
                 return
             elif target == 0:
@@ -11,6 +11,6 @@ class Solution(object):
             for i in xrange(start, len(nums)):
                 if helper(i+1, target-nums[i]):
                     return True
-            cache[(start, target)] = False
+            memo.add((start, target))
             return False
         return False if sum(nums)%2 else helper(0, sum(nums)/2)
