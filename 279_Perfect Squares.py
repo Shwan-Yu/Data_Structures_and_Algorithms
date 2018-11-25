b9838d9f727fe4cbd7ef4a04c21c1871d4f496d9
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-11-25 17:29:49 -0500

    Create 279_Perfect Squares.py

---
+++ 279_Perfect Squares.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0]+ [float("inf")] * n
+        for i in range(1, int(n**0.5)+1):
+            for j in range(i**2, n+1):
+                dp[j] = min(dp[j], dp[j-i**2]+1)
+        return dp[-1]
