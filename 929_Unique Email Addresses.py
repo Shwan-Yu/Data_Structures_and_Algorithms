e67f576085eecc58f767a6e4fd9389b7432b88f7
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-12-08 17:25:51 -0500

    Create 929_Unique Email Addresses.py

---
+++ 929_Unique Email Addresses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        memo = set()
+        for email in emails:
+            loc, dom = email.split("@")
+            if "+" in loc: loc = loc[:loc.index("+")]
+            loc = "".join(loc.split("."))
+            memo.add(loc+dom)
+        return len(memo)
