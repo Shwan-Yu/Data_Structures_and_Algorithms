5c9fcf11bdb33fe7b282cf1eaed3e04ad9ec5416
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-12-09 22:08:10 -0500

    Update 076_Minimum Window Substring.py

--- 076_Minimum Window Substring.py
+++ 076_Minimum Window Substring.py
@@ -6,20 +6,19 @@ class Solution(object):
         :rtype: str
         """
         left, right, res_l, res_r, min_len = 0, 0, 0, 0, float("inf")
-        count_map = collections.defaultdict(int)
-        for char in t: 
-            count_map[char] += 1
+        count = collections.Counter(t)
         while right <= len(s):
-            if all(map(lambda i: True if i <= 0 else False, count_map.values())):
+            if all(map(lambda i: True if i <= 0 else False, count.values())):
                 if (right - left) < min_len:
                     min_len = right - left
                     res_l, res_r = left, right
-                if s[left] in count_map: 
-                    count_map[s[left]] += 1
+                if s[left] in count: 
+                    count[s[left]] += 1
                 left += 1
             else:
                 if right == len(s): break
-                if s[right] in count_map: 
-                    count_map[s[right]] -= 1
+                if s[right] in count: 
+                    count[s[right]] -= 1
                 right += 1
         return s[res_l:res_r]
+                   
