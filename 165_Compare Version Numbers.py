9352caff6be13ededd7150c650989ac320a8a9d8
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-12-10 21:28:44 -0500

    Update 165_Compare Version Numbers.py

--- 165_Compare Version Numbers.py
+++ 165_Compare Version Numbers.py
@@ -8,6 +8,7 @@ class Solution(object):
         version1 = map(int, version1.split("."))
         version2 = map(int, version2.split("."))
         for i in range(max(len(version1), len(version2))):
+            # complete with 0 because we can have 1.00000 and 1.00 or 1.0001 and 1.00
             v1 = version1[i] if i < len(version1) else 0
             v2 = version2[i] if i < len(version2) else 0
             if v1 > v2: return 1
