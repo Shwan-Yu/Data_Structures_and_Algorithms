12d380973b03d5787bca9bc91aa804a9e1fcade8
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-12-15 15:58:18 -0500

    Create 166_Fraction to Recurring Decimal.py

---
+++ 166_Fraction to Recurring Decimal.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fractionToDecimal(self, numerator, denominator):
+        """
+        :type numerator: int
+        :type denominator: int
+        :rtype: str
+        """
+        # get the n and remainder, record it in a string, then using stack to track if repeating
+        sign = "-" if numerator * denominator < 0 else ""
+        ans, remain = divmod(abs(numerator), abs(denominator))
+        if remain == 0: return sign + str(ans)
+        res = [sign + str(ans) + "."]
+        stack = []
+        while remain not in stack:
+            stack.append(remain)
+            ans, remain = divmod(remain*10, abs(denominator))
+            res.append(str(ans))
+        res.insert(stack.index(remain)+1 ,"(")
+        res.append(")")
+        return "".join(res).replace("(0)", "")
