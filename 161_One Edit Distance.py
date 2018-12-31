665c061986826a6cb9ee255b8c7cf2c9e815a370
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2018-12-31 16:59:36 -0500

    Update 161_One Edit Distance.py

--- 161_One Edit Distance.py
+++ 161_One Edit Distance.py
@@ -5,15 +5,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if abs(len(s)-len(t))>1: return False
+        # faster with shorter slicing
+        if abs(len(s)-len(t))>1 or s == t: return False
         if len(s)-len(t) == -1:
-            return True if any(t[:i]+t[i+1:] == s for i in range(len(t))) else False
+            for i in range(len(s)):
+                if s[i]!=t[i]: return s[i:] == t[i+1:]
         elif len(s)-len(t) == 1:
-            return True if any(s[:i]+s[i+1:] == t for i in range(len(s))) else False
+            for i in range(len(t)):
+                if s[i]!=t[i]: return t[i:] == s[i+1:]
         else:
-            # for i in range(len(s)):
-            #     if s[i] != t[i]: return s[i+1:] == t[i+1:]
-            # return False
-            count = sum(s[i] != t[i] for i in range(len(s)))
-            return True if count == 1 else False
+            for i in range(len(t)):
+                if s[i]!=t[i]: return t[i+1:] == s[i+1:]
+        return True
+        
+        # if abs(len(s)-len(t))>1: return False
+        # if len(s)-len(t) == -1:
+        #     return True if any(t[:i]+t[i+1:] == s for i in range(len(t))) else False
+        # elif len(s)-len(t) == 1:
+        #     return True if any(s[:i]+s[i+1:] == t for i in range(len(s))) else False
+        # else:
+        #     # for i in range(len(s)):
+        #     #     if s[i] != t[i]: return s[i+1:] == t[i+1:]
+        #     # return False
+        #     count = sum(s[i] != t[i] for i in range(len(s)))
+        #     return True if count == 1 else False
                 
