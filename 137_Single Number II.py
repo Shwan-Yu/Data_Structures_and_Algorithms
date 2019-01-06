4aa7a0ab676fb67cab410aa5dca0ab46d3b137d6
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2019-01-06 17:53:08 -0500

    Update 137_Single Number II.py

--- 137_Single Number II.py
+++ 137_Single Number II.py
@@ -4,6 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # e.g. 2 2 2 3
+        # 0010 0010 0010 0011
+        #   a      b
+        # 0010    0000
+        # 0000    0010
+        # 0000    0000
+        # a ^ num & 1111 -> ~b
+        # a ^ num & xxxx -> ~b
+        # a ^ num & ~(a^num) -> ~b
+        
+        # b ^ num & ~(b^num) -> ~a
+        # ...
+        
         a = b = 0
         for num in nums:
             a = a ^ num & ~b
