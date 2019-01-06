0f7cbb304897ecb1ef538cdab152888a5b3bb03c
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2019-01-06 12:02:25 -0500

    Update 078_Subsets.py

--- 078_Subsets.py
+++ 078_Subsets.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # 2^n -1
         res = [[]]
         for num in nums:
             res += [ele + [num] for ele in res]
         return res
         
         # or
-        
+        # 2^n doubled at every time
 #         def backtracking(nums, index, path, res):
 #             res.append(path)
 #             for i in range(index, len(nums)):
