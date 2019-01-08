f28776514861ff4dd06527d5dcada26b285002a7
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2019-01-08 12:27:13 -0500

    Create 081_Search in Rotated Sorted Array II.py

---
+++ 081_Search in Rotated Sorted Array II.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        # 1 3 1 1 1
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target: return True
+            while l < mid and nums[l] == nums[mid]: # tricky part
+                l += 1
+            if nums[l]<=nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid -1
+                else:
+                    l = mid +1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid +1
+                else:
+                    r = mid -1
+        return False
