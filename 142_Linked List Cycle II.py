1b7abfd8a70a2b4a31ab11b782f552fcf857ab8b
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2019-01-08 16:59:17 -0500

    Update 142_Linked List Cycle II.py

--- 142_Linked List Cycle II.py
+++ 142_Linked List Cycle II.py
@@ -10,10 +10,26 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        # O(n)
-        memo = set()
-        while head:
-            if head in memo: return head
-            memo.add(head)
+        # meeting point k: 2K-K = nC
+        # from circle start to meeting point: m: res_dis = K-m = nC-m = (n-1)C + (C-m)
+        # make n = 1, from start to starting point = from meeting point to starting point
+        
+        fast = slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: break
+        else: return None
+        while head != slow:
             head = head.next
-        return None
+            slow = slow.next
+        return head
+        
+        # O(n)
+        # memo = set()
+        # while head:
+        #     if head in memo: return head
+        #     memo.add(head)
+        #     head = head.next
+        # return None
+        
