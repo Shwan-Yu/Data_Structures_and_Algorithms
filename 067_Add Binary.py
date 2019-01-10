72cb49dbaae2db019dd88a9b0e18a47644286662
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2019-01-10 11:22:27 -0500

    Update 067_Add Binary.py

--- 067_Add Binary.py
+++ 067_Add Binary.py
@@ -5,14 +5,27 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        #unicode
-        res, carry = "", 0
-        while len(a) != len(b):
-            if len(a)<len(b): a = "0" + a
-            else: b = "0" + b  
-        for i in reversed(range(len(a))):
-            total = ord(a[i]) - ord("0") + ord(b[i]) - ord("0") + carry
-            res = `total%2`  + res
-            carry = total // 2
-        if carry: res = "1" + res
+        a, b = a[::-1], b[::-1]
+        i, carry, res = 0, 0, ""
+        while i < max(len(a), len(b)) or carry:
+            v1 = v2 = 0
+            if i < len(a):
+                v1 = ord(a[i]) - ord("0")
+            if i < len(b):
+                v2 = ord(b[i]) - ord("0")
+            carry, remainder = divmod(v1+v2+carry, 2)
+            res = str(remainder) + res
+            i += 1
         return res
+    
+        #unicode
+#         res, carry = "", 0
+#         while len(a) != len(b):
+#             if len(a)<len(b): a = "0" + a
+#             else: b = "0" + b  
+#         for i in reversed(range(len(a))):
+#             total = ord(a[i]) - ord("0") + ord(b[i]) - ord("0") + carry
+#             res = `total%2`  + res
+#             carry = total // 2
+#         if carry: res = "1" + res
+#         return res
