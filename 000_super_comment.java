64e17d7cb56fad2ea74497d0cb02fa30b4c680f3
Shwan-Yu <42786480+Shwan-Yu@users.noreply.github.com>
2019-08-08 17:03:15 +0800

    Update 000_super_comment.java

--- 000_super_comment.java
+++ 000_super_comment.java
@@ -16,3 +16,9 @@
 //                 \ \ `-. \_ __\ /__ _/ .-` / /  
 //         ======`-.____`-.___\_____/___.-`____.-'======  
 //                            `=---='
+
+//Cloud Business Unit
+// Software Engineer Intern   May 2018 – Aug 2018
+// Collaborate with test data collection teams and developed scalable message queues to implement business logic decoupling and peak clipping for team’s auto-testing service via Kafka.
+// Built stream computing framework independently via Flink, hence provided real-time test log data preprocessing and cleaning with 5-30 milliseconds delay.
+// Improved Flink stream processing speed by 60% based on the business and product features.
